269e7b987a6a3a9ecff06030e8801d40696b4fd9
Mert Dede <doganmertdede@gmail.com>
2024-04-12 08:54:12 +0300

    Cleanup and update ToggleMode

--- app/(home)/sections/generic-input.tsx
+++ app/(home)/sections/generic-input.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Form,
@@ -10,9 +11,7 @@ import {
 } from "@/components/ui/form";
 import { PhoneInput } from "@/components/ui/phone-input";
 import { toast } from "@/components/ui/use-toast";
-import { siteConfig } from "@/config/site";
 import { zodResolver } from "@hookform/resolvers/zod";
-import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { isValidPhoneNumber } from "react-phone-number-input";
 import { z } from "zod";
@@ -21,7 +20,7 @@ const FormSchema = z.object({
   phone: z.string().refine(isValidPhoneNumber, { message: "Invalid phone number" }),
 });
 
-export default function Hero() {
+export default function Showcase() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
@@ -42,34 +41,6 @@ export default function Hero() {
 
   return (
     <>
-      <section className="z-10 max-w-5xl w-full flex flex-col items-center text-center gap-5">
-        <div className="z-10 w-full flex flex-col items-center text-center gap-5">
-          <h1 className="scroll-m-20 text-4xl font-bold tracking-tight">Shadcn Phone Input</h1>
-          <p className="text-muted-foreground max-w-[450px]">
-            An implementation of a Phone Input component built on top of Shadcn UI&apos;s input component.
-          </p>
-          <div className="flex gap-2 mt-1">
-            <Link
-              href="#try"
-              className={`${buttonVariants({
-                variant: "default",
-                size: "lg",
-              })} min-w-[150px] shadow-sm`}
-            >
-              Try it out
-            </Link>
-            <Link
-              href={siteConfig.links.github}
-              className={`${buttonVariants({
-                variant: "secondary",
-                size: "lg",
-              })} shadow-sm`}
-            >
-              Github
-            </Link>
-          </div>
-        </div>
-
         <div id="try" className="w-full py-8">
           <div className="w-full relative my-4 flex flex-col space-y-2">
             <div className="preview flex min-h-[350px] w-full justify-center p-10 items-start mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 relative rounded-md border">
@@ -91,14 +62,18 @@ export default function Hero() {
                   />
                   <pre>
                     <code className="text-foreground">{JSON.stringify(form.watch("phone"), null, 2)}</code>
-                  </pre>
+                </pre>
+                
+                <div className="flex flex-row gap-3">
                   <Button type="submit">Submit</Button>
+                  <ModeToggle />
+                </div>
+                  
                 </form>
               </Form>
             </div>
           </div>
         </div>
-      </section>
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,43 +10,6 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Shadcn Phone Input",
-  description: `A phone input component implementation of Shadcn's input component`,
-  keywords: [
-    "shadcn",
-    "phone input",
-    "shadcn/ui",
-    "shadcn phone input",
-    "phone input component",
-    "shadcn phone input component",
-    "input",
-    "radix ui",
-    "react phone input",
-  ],
-  authors: [
-    {
-      name: "Omer Alpi",
-      url: "https://jaleelbennett.com",
-    },
-  ],
-  creator: "Omer Alpi",
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    creator: "@omeralpi",
-  },
 };
 
 export default function RootLayout({
